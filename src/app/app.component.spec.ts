60b85a54d3004f2935d43bf5b290802ef513d77b
Filipe Nogueira <84330960+Filipendsa@users.noreply.github.com>
2024-02-22 10:22:45 -0300

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'patrimony-management' title`, () => {
+  it(`should have the 'patrimony' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('patrimony-management');
+    expect(app.title).toEqual('patrimony');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, patrimony-management');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, patrimony');
   });
 });
