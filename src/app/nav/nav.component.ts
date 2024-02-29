7a1632aeaea79526b38a47ca3ce46d30e02a1561
Filipe Nogueira <84330960+Filipendsa@users.noreply.github.com>
2024-02-29 20:23:02 -0300

    Add ngx-skeleton-loader and primeicons dependencies, update nav component, and add menu.json

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { FormsModule } from '@angular/forms';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { MenuItem } from 'primeng/api';
+
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [CardModule, FormsModule],
+  imports: [CardModule, FormsModule,PanelMenuModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
   public text: string = 'Hello World';
-
-  ngOnInit() {
-  }
+  @Input() menuItens:MenuItem[] = [];;
+  constructor() {}  
 }
