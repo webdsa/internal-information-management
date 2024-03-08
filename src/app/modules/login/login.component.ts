4fdf8d92d454c52178d1f445760d0be51a5345db
Mikaela Brito <brito.mikaela36@gmail.com>
2024-03-08 13:30:00 -0300

    fix: style form login

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  user = {
+    email: '',
+    password: ''
+  };
+
+  onSubmit() {
+    console.log('Formulário enviado:', this.user);
   }
 
 }
