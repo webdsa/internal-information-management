f9b46a0b479b9ba60a4316fa136cae4f0ae0c95a
Mikaela Brito <brito.mikaela36@gmail.com>
2024-03-08 13:29:48 -0300

    feat: implementation ngModule

---
+++ src/app/modules/modules.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+    declarations: [LoginComponent],
+    imports: [FormsModule, ReactiveFormsModule],
+    exports: [LoginComponent]
+})
+
+export class ModulesModule {}
