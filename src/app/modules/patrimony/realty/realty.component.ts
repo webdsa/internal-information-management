611fb65cc4b853aba80a9d8273d36e91fb49e066
Filipe Nogueira <84330960+Filipendsa@users.noreply.github.com>
2024-04-09 20:12:45 -0300

    Refactor modules-routing.module.ts to add RealtyComponent route and update property route

---
+++ src/app/modules/patrimony/realty/realty.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-realty',
+  standalone: true,
+  imports: [],
+  templateUrl: './realty.component.html',
+  styleUrl: './realty.component.scss'
+})
+export class RealtyComponent {
+
+}
