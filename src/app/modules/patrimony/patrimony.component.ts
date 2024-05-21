72d63ed95af9c95ab5e8296ce1d5044655893e4d
Filipe Nogueira <84330960+Filipendsa@users.noreply.github.com>
2024-05-20 21:07:39 -0300

    Refactor patrimony component to include user name in welcome message

--- src/app/modules/patrimony/patrimony.component.ts
+++ src/app/modules/patrimony/patrimony.component.ts
@@ -4,21 +4,62 @@ import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 import { layoutMenu } from '../../core/models/layoutMenu.model';
 import { HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from '../../shared/menu-bar/menu-bar.component';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-patrimony',
   standalone: true,
-  imports: [NavComponent,NgxSkeletonLoaderModule,RouterOutlet],
+  imports: [NavComponent, NgxSkeletonLoaderModule, RouterOutlet, MenuBarComponent],
   templateUrl: './patrimony.component.html',
   styleUrl: './patrimony.component.scss'
 })
 export class PatrimonyComponent {
 
-  public menuItens:layoutMenu[] = [];
-  public colapse:number = 300;
-  constructor(private _httpClient: HttpClient) {   }
+  public menuItens: layoutMenu[] = [];
+  public colapse: number = 300;
+  public personName: string = 'User';
+  public reports: any[] = [ // Change the type from Array<any> to any[]
+    {
+      "id": 0,
+      "name": "IPTU",
+      "icon": "description",
+    },
+    {
+      "id": 1,
+      "name": "Fotovoltaica",
+      "icon": "description",
+    },
+    {
+      "id": 2,
+      "name": "Energia",
+      "icon": "description",
+    },
+    {
+      "id": 3,
+      "name": "Gás",
+      "icon": "description",
+    },
+    {
+      "id": 4,
+      "name": "Conta de água",
+      "icon": "description",
+    },
+    {
+      "id": 5,
+      "name": "Condomínio",
+      "icon": "description",
+    },
+    {
+      "id": 6,
+      "name": "Imóveis próprios",
+      "icon": "description",
+    }
+  ];
+  constructor(private _httpClient: HttpClient, private _sanitizer: DomSanitizer) { }
   ngOnInit() {
-    
+
+    this.personName = localStorage.getItem('user.name')!;
     this._httpClient.get<layoutMenu[]>('assets/mock/menu.json').subscribe(data => {
       this.menuItens = data;
     });
