2a9f7479deac7d13018a9d7b2a26eebff6447f00
Filipe Nogueira <84330960+Filipendsa@users.noreply.github.com>
2024-06-20 11:59:21 -0300

    refactor: Update environment.ts with new redirect and urlApi values

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,13 +2,14 @@ export const environment = {
     msalConfig: {
         clientId: '3ecdf6fc-e97b-4fec-87ab-827a54f6d471',
         tenantId: 'f8cdef31-a31e-4b4a-93e4-5f571e91255a',
-        redirect: 'https://d286-187-75-79-100.ngrok-free.app'
+        // redirect: 'https://8616-177-39-104-48.ngrok-free.app' // use this for production
+        redirect: 'http://localhost:4200' // use this for local
     },
     graph: {
         uri: 'https://graph.microsoft.com/v1.0/me',
         scopes: ['user.read']
     },
-    urlApi: 'https://d286-187-75-79-100.ngrok-free.app'
+    urlApi: 'https://e4b4-179-89-244-234.ngrok-free.app'
     // urlApi: 'http://localhost:5171'
 
 };
