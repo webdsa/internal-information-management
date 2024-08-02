4d69619b29ec469390eb0c7c530f2e7b134c64a5
Filipe Nogueira <84330960+Filipendsa@users.noreply.github.com>
2024-08-02 11:00:31 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,8 +9,8 @@ export const environment = {
         uri: 'https://graph.microsoft.com/v1.0/me',
         scopes: ['user.read']
     },
-    urlApi: 'https://782d-2804-214-8593-5218-60d9-db6-d9fd-9734.ngrok-free.app'
+    //urlApi: 'https://782d-2804-214-8593-5218-60d9-db6-d9fd-9734.ngrok-free.app'
     // urlApi: 'http://localhost:5171'
-    //urlApi: 'https://mgn.dsa.org.br'
+    urlApi: 'https://mgn.dsa.org.br'
 
 };
