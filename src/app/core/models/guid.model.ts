d4e40bb17b6c556e4581c9d698d4e76117c6b528
Filipe Nogueira <84330960+Filipendsa@users.noreply.github.com>
2024-08-21 09:03:17 -0300

    fix: Update GuidModel and Subtopics classes
    The GuidModel and Subtopics classes have been updated to include additional properties and fix existing ones. This ensures that the data is accurately represented and improves the functionality of the application.

--- src/app/core/models/guid.model.ts
+++ src/app/core/models/guid.model.ts
@@ -1,29 +1,28 @@
 export class GuidModel {
-    id!: number;
-    name!: string;
-    isActive!: boolean;
-    subTopics!: Subtopics[];
-    rules!: Rules[];
+  id!: number;
+  name!: string;
+  isActive!: boolean;
+  subTopics!: Subtopics[];
+  rules!: Rules[];
 }
 
 export class Subtopics {
-    id!: number;
-    name!: string;
-    description!: string;
-    content!: string;
-    isActive!: boolean;
-    topicId!: number;
-    topic!: string;
-    rules!: Rules[];
+  id!: number;
+  name!: string;
+  description!: string;
+  content!: string;
+  isActive!: boolean;
+  topicId!: number;
+  topic!: string;
+  rules!: Rules;
 }
 
 export class Rules {
-    id!: number;
-    ruleFactor!: string;
-    ruleTypeId!: number;
-    topicId!: number;
-    subTopicId!: number;
-    ruleType!: any;
-    topic!: string;
-    subTopic!: string;
+  id!: number;
+  topicId!: number;
+  subTopicId!: number;
+  isReligious!: boolean;
+  maxOrderOcupational!: number;
+  minOrderOcupational!: number;
+  functionalPropertyName!: [string];
 }
--- src/app/modules/internal-information/components/alter-rule-subtopic/alter-rule-subtopic.component.ts
+++ src/app/modules/internal-information/components/alter-rule-subtopic/alter-rule-subtopic.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output, WritableSignal, inject, signal } from '@angular/core';
+import { Component, EventEmitter, Input, InputSignal, Output, WritableSignal, inject, input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -7,28 +7,28 @@ import { FloatLabelModule } from 'primeng/floatlabel';
 import { ToastrService } from 'ngx-toastr';
 
 import { InternalService } from '../../services/internal.service';
-import { Subtopics } from '../../../../core/models/guid.model';
+import { Rules, Subtopics } from '../../../../core/models/guid.model';
 
 @Component({
   selector: 'app-alter-rule-subtopic',
   standalone: true,
-  imports: [CommonModule, FormsModule,MultiSelectModule,FloatLabelModule],
+  imports: [CommonModule, FormsModule, MultiSelectModule, FloatLabelModule],
   templateUrl: './alter-rule-subtopic.component.html',
   styleUrl: './alter-rule-subtopic.component.scss'
 })
 export class AlterRuleSubtopicComponent {
-  protected form: WritableSignal<TravelerProfile> = signal<TravelerProfile>(new TravelerProfile());
+  protected form: WritableSignal<Rules> = signal<Rules>(new Rules());
 
-  @Input() public subtopic: Subtopics = new Subtopics();
+  public subtopic: InputSignal<Subtopics> = input(new Subtopics());
   @Output() onClose: EventEmitter<boolean> = new EventEmitter<boolean>();
   protected propertys: WritableSignal<string[]> = signal([]);
   #InternalService = inject(InternalService);
   constructor(private _toast: ToastrService) {}
 
   ngOnInit(): void {
-    this.form().topicId = this.subtopic.topicId;
-    console.log(this.subtopic.topicId,'woow')
-    this.form().subTopicId = this.subtopic.id;
+    this.form.set(this.subtopic().rules);
+    this.form().topicId = this.subtopic().topicId;
+    this.form().subTopicId = this.subtopic().id;
     this.getPropertys();
   }
   public AlterRuleSubTopic() {
@@ -57,15 +57,4 @@ export class AlterRuleSubtopicComponent {
   selectProperty(target: any) {
     this.form().functionalPropertyName = target.value;
   }
-  callMe(){
-    console.log(this.form(),'like u');
-  }
-}
-export class TravelerProfile {
-  topicId!: number;
-  subTopicId!: number;
-  isReligious!: boolean;
-  maxOrderOcupational!: number;
-  minOrderOcupational!: number;
-  functionalPropertyName!: string;
 }
